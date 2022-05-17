07c66b351d98019af034f4ffa6a90df8a3b174f5
Vitaly Medvedev <vemedvedev@fil-it.ru>
2022-05-17 18:54:34 +0300

    start write some mapper

--- src/main/java/ru/homework/andry/soap/mapper/EmployeeMapper.java
+++ src/main/java/ru/homework/andry/soap/mapper/EmployeeMapper.java
@@ -1,10 +1,16 @@
 package ru.homework.andry.soap.mapper;
 
 import io.dliga.micro.employee_web_service.Employee;
+import io.dliga.micro.employee_web_service.Position;
 import org.mapstruct.Mapper;
+import ru.homework.andry.soap.model.AbstractEmployee;
+import ru.homework.andry.soap.model.AnalyticsElement;
+import ru.homework.andry.soap.model.DeveloperElement;
+import ru.homework.andry.soap.model.ManagerElement;
 import ru.homework.andry.soap.repository.entity.EmployeeEntity;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", uses = Employee.class)
 public interface EmployeeMapper {
@@ -14,4 +20,26 @@ public interface EmployeeMapper {
     List<EmployeeEntity> employeesToEmployeesEntity(List<Employee> employees);
 
     List<Employee> employeesEntityToEmployees(List<EmployeeEntity> employees);
+
+    AnalyticsElement employeeToAnalytics(Employee employee);
+
+    DeveloperElement employeeToDeveloper(Employee employee);
+
+    ManagerElement employeeToManager(Employee employee);
+
+    default List<AbstractEmployee> mapEmployeesToCurrentEmployeesByPosition(List<Employee> employees) {
+        return employees.stream().map(this::mapWSEmployee).collect(Collectors.toList());
+    }
+
+    private AbstractEmployee mapWSEmployee(Employee employee) {
+        if (employee.getPosition().equals(Position.ANALYTICS)) {
+            return employeeToAnalytics(employee);
+        }
+
+        if (employee.getPosition().equals(Position.DEVELOPER)) {
+            return employeeToDeveloper(employee);
+        }
+
+        return employeeToManager(employee);
+    }
 }
--- src/test/java/ru/homework/andry/soap/mapper/EmployeeEntityMapperTest.java
+++ src/test/java/ru/homework/andry/soap/mapper/EmployeeEntityMapperTest.java
@@ -1,20 +1,18 @@
 package ru.homework.andry.soap.mapper;
 
-import io.dliga.micro.employee_web_service.GetEmployeesResponse;
+import io.dliga.micro.employee_web_service.Employee;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
-import ru.homework.andry.soap.repository.entity.EmployeeEntity;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static ru.homework.andry.soap.testdata.EmployeesTestData.employeesToTestMapper;
 
 import java.util.List;
+
+import static ru.homework.andry.soap.testdata.EmployeesTestData.employeesToTestMapper;
 
 class EmployeeEntityMapperTest {
 
     private final EmployeeMapper employeeMapper = Mappers.getMapper(EmployeeMapper.class);
 
-/*    @Test
+/*  @Test
     void employeesToGetEmployeesResponse_twoEmpEnt_responseWith2elemEmployee() {
         List<EmployeeEntity> employeeEntities = employeesToTestMapper();
 
@@ -27,4 +25,10 @@ class EmployeeEntityMapperTest {
             assertEquals(employeeEntities.get(i).getLastname(), getEmployeesResponse.getEmployees().get(i).getLastname());
         }
     }*/
+
+    @Test
+    void mapEmployeesToCurrentEmployeesByPosition() {
+        List<Employee> employeesFromWS = employeesToTestMapper();
+
+    }
 }
